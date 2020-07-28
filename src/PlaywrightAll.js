6d14be6be2df67e7a044b7f7026131c75f5c3b2d
sposhe <github@seanoshea.me>
2020-07-27 21:28:53 -0400

    Shift registration into All

--- src/PlaywrightAll.js
+++ src/PlaywrightAll.js
@@ -1,5 +1,19 @@
-export { PlaywrightElement   } from './PlaywrightElement.js'
-export { PlaywrightDirection } from './PlaywrightDirection.js'
-export { PlaywrightPersona   } from './PlaywrightPersona.js'
-export { PlaywrightSpeaker   } from './PlaywrightSpeaker.js'
-export { PlaywrightDialog    } from './PlaywrightDialog.js'
+import { PlaywrightElement   } from './PlaywrightElement.js'
+import { PlaywrightDirection } from './PlaywrightDirection.js'
+import { PlaywrightPersona   } from './PlaywrightPersona.js'
+import { PlaywrightSpeaker   } from './PlaywrightSpeaker.js'
+import { PlaywrightDialog    } from './PlaywrightDialog.js'
+
+customElements.define('pw-element',   PlaywrightElement   )
+customElements.define('pw-direction', PlaywrightDirection )
+customElements.define('pw-persona',   PlaywrightPersona   )
+customElements.define('pw-speaker',   PlaywrightSpeaker   )
+customElements.define('pw-dialog',    PlaywrightDialog    )
+
+export {
+  PlaywrightElement,
+  PlaywrightDirection,
+  PlaywrightPersona,
+  PlaywrightSpeaker,
+  PlaywrightDialog
+}
